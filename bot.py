dd0735a94304305629e55f4d199e67ee8227aa76
Mohamad Nour Ali <76864296+nor2052@users.noreply.github.com>
2026-04-01 21:42:31 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -99,7 +99,7 @@ async def handle_messages(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     summary = summarize(message.text)
 
-    await message.reply_text(f" **قَالَ المُحَشِّي الفَاضِل:** : \n\n{summary}")
+    await message.reply_text(f" **قَالَ المُحَشِّي الفَاضِل:**:\n\n{summary}")
 
 # 🚀 تشغيل البوت
 def main():
